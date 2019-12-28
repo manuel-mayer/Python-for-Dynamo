3587e5c4358fcc42f86ea04e1ad9a92d53619870
Manuel Mayer <59269608+manuel-mayer@users.noreply.github.com>
2019-12-28 17:35:31 +0100

    Update AreaInGoldenRatio_ListInput.py

--- AreaInGoldenRatio_ListInput.py
+++ AreaInGoldenRatio_ListInput.py
@@ -1,26 +1,26 @@
-#calculates length and width of a rectangle in the golden ratio, based on the given area, input as list
+# calculates length and width of a rectangle in the golden ratio, based on the given area, input as list
 
 import math
 
 area = IN[0] #input has to be a list
 
-#convert inputlist to float
+# convert inputlist to float
 area_fl = []
 for i in range(len(area)):
 	result_a = float(area[i])
 	area_fl.append(result_a)
 
-#calculate golden angle
+# calculate golden angle
 phi = (1+math.sqrt(5))/2
 
-#calculate width
+# calculate width
 width_out = []
 for f in area_fl:
 	width = math.sqrt(f*(1/phi))
 	width_out.append(width)
 width_r = ['%.1f' % elem for elem in width_out] #round to 1 decimal
 
-#calculate length
+# calculate length
 length_out = []
 for a,w in zip(area_fl,width_out):
 	length = a/w
